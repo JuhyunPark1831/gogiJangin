4637081e04680f8e1d729cb61557f6019f662221
JuhyunPark <epahs1831@naver.com>
2026-06-09 14:51:49 +0900

    Feat [2026-06-09] 박주현
    - 이슈 테스트
    
    -#5

--- src/main/java/com/project/gogiJangin/service/impl/FranchiseServiceImpl.java
+++ src/main/java/com/project/gogiJangin/service/impl/FranchiseServiceImpl.java
@@ -73,5 +73,5 @@ public class FranchiseServiceImpl implements FranchiseService {
                 .first(franchisePage.isFirst())
                 .last(franchisePage.isLast())
                 .build();
-    } 
+    }
 }
