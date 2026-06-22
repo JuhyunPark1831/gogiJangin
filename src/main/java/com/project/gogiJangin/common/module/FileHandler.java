5c202d68c24b0ca66f4a2268326b4670aedc50bf
JuhyunPark <epahs1831@naver.com>
2026-06-22 22:05:26 +0900

    Feat [2026-06-22] 박주현 - 파일 저장 기능 개발중
    -#8

--- src/main/java/com/project/gogiJangin/common/module/FileHandler.java
+++ src/main/java/com/project/gogiJangin/common/module/FileHandler.java
@@ -1,4 +1,39 @@
 package com.project.gogiJangin.common.module;
 
+import com.project.gogiJangin.repository.AttachFileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class FileHandler {
+
+    private final AttachFileRepository attachFileRepository;
+
+    public static boolean fileCheck(String filePath, String fileOriName) {
+
+        File file = new File(filePath + File.separator + fileOriName);
+
+        return file.exists();
+    }
+
+    public static Long saveFiles(List<MultipartFile> fileList, String filePath) {
+
+        try {
+            for (MultipartFile file : fileList) {
+                String savePath = filePath + File.separator + System.currentTimeMillis() + file.getOriginalFilename();
+                File saveFile = new File(savePath);
+                image.transferTo(file);
+            }
+
+            return filePath;
+        } catch (IOException e) {
+            throw new CustomException(ErrorCode.FILE_SAVE_FAIL);
+        }
+    }
 }
